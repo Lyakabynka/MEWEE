4df8c87ebab5075499101517cae1031eced4365f
Andriy31193 <andriycreator@gmail.com>
2024-03-16 12:38:14 +0000

    ff

---
+++ src/Backend/MEWEE/IdentityService.Application/Mediatr/User/Commands/RegisterCommand.cs
@@ -0,0 +1,11 @@
+﻿using IdentityService.Application.Features.Interfaces;
+using IdentityService.Application.Response;
+
+namespace IdentityService.Application.Mediatr.User.Commands;
+
+public class RegisterCommand : IValidatableRequest<Result>
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+}
---
+++ src/Backend/MEWEE/IdentityService.WebApi/Controllers/ApiControllerBase.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Claims;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityService.WebApi.Controllers;
+
+[ApiController]
+public abstract class ApiControllerBase : ControllerBase
+{
+    private IMediator? _mediator;
+
+    protected IMediator Mediator =>
+        _mediator ??= HttpContext.RequestServices.GetRequiredService<IMediator>();
+    
+    protected internal Guid UserId => User.Identity?.IsAuthenticated is true
+        ? Guid.Parse(User.FindFirstValue("userId")!)
+        : Guid.Empty;
+}
