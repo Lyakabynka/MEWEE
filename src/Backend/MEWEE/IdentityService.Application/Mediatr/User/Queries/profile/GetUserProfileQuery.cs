9333f6d571c44a42901ca5df5a487a09c26f1147
Andriy31193 <andriycreator@gmail.com>
2024-03-22 14:47:45 +0000

    front-update

---
+++ src/Backend/MEWEE/IdentityService.Application/Mediatr/User/Queries/profile/GetUserProfileQuery.cs
@@ -0,0 +1,9 @@
+﻿using IdentityService.Application.Features.Interfaces;
+using IdentityService.Application.Response;
+
+namespace IdentityService.Application.Mediatr.User.Queries;
+
+public class GetUserProfileQuery : IValidatableRequest<Result>
+{
+    public Guid UserId { get; set; }
+}
